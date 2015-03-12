16e176585da0bed4e33d860aa4067d3f6347eea0
hkal <_@hkal.me>
2015-03-11 23:24:38 -0400

    First

---
+++ test/problem1.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var solution = require('../src/problem1');
+
+describe('The solution for Problem 1', function() {
+    it('should return 233168', function() {
+        assert(solution('1000', 233168));
+    })
+});

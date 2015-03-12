145aff99eaf0b706e64eddcd4d352e9c9d985c2d
hkal <_@hkal.me>
2015-03-12 00:43:01 -0400

    Solved Problem 1
    Lessons Learned:
    
      * TDD only works if you write accurate tests
      * Javascript needs a native 'range()' function
      * 'new Array(n)' creates a sparse array. Use Array.apply() instead?

--- src/problem1.js
+++ src/problem1.js
@@ -1,1 +1,11 @@
-module.exports = [];
+module.exports = function(limit) {
+    return Array
+        .apply(null, Array(limit))
+        .map(Function.prototype.call.bind(Number))
+        .filter(function(x) {
+            return (x % 3 === 0 || x % 5 === 0);
+        })
+        .reduce(function(previous, current) {
+            return previous + current;
+        });
+};
--- test/problem1.spec.js
+++ test/problem1.spec.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var solution = require('../src/problem1');
 
-describe('The solution for Problem 1', function() {
-    it('should return 233168', function() {
-        assert(solution('1000', 233168));
+describe('Problem 1 solution', function() {
+    it('should equal 233168', function() {
+        assert.equal(solution(1000), 233168);
     })
 });
